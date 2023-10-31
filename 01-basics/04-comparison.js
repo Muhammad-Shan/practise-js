f7e6906d20cc89ffdfcba6ce28f85c63be7f0172
Muhammad Shan <134515333+Muhammad-Shan@users.noreply.github.com>
2023-10-31 10:33:44 +0000

    comparison

---
+++ 01-basics/04-comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // Problem not compare same datatype
+// console.log("02" > 1); // Automatic JavaScript convert 02 in number
+
+
+// Avoid this type comparison
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); //this case fill 0 in null value so 0 = 0 is show true
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// Avoid this type comparison
+
+
+console.log("2" === 2); // === check value and datatype strickly
