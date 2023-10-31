80df72e84bc1ee904f1dc0b0a634b62fe8d20c00
Muhammad Shan <134515333+Muhammad-Shan@users.noreply.github.com>
2023-10-31 06:05:49 +0000

    conversion of datatype

---
+++ 01-basics/03-conversionoperation.js
@@ -0,0 +1,33 @@
+let score = "33"
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score) // string conert in number
+//console.log(typeof valueInNumber)
+
+
+
+// Notes
+// "33" => 33 // string convert in number
+// "33abc" => // Not convert this type of string provide "NaN stands for Not A Number"
+// true => // convert in 1 & false conert in 0
+
+//let isLoggedIn = 1
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+// Notes
+// 1 => true & 0 => false
+// "" => false //empty strings show false
+// "Shan" //value add in string then show true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
