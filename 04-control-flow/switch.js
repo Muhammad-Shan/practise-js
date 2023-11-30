4764933d73ec5ab9d96ae4be1c8b4a8fe5acfe89
Muhammad Shan <134515333+Muhammad-Shan@users.noreply.github.com>
2023-11-30 11:26:36 +0500

    switch

---
+++ 04-control-flow/switch.js
@@ -0,0 +1,51 @@
+// Values ko multiple condition sye check karna kye lye "Switch Statement" use hota ha
+
+// const month = 3
+
+// switch (month) { // "key" wo value ha jo har bar check karni ha
+//     case 1:
+//         console.log("January"); // "shift, Alt or Down Arrow" to duplicate text in next line
+//         break;  
+
+//     case 2:
+//         console.log("February");
+//         break; // 1 bar jaha per bhi "case match" ho gye us kye bad ka sara "code execute" karta ha "except default" kye // Agar "Break" na hota to "baki cases" ko match hii nhi karta
+
+//     case 3:
+//         console.log("March");
+//         break;
+        
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Default Case Match");
+//         break;
+// }
+
+
+// show "String value"
+const month = "April"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;  
+
+    case "February":
+        console.log("February");
+        break; 
+
+    case "March":
+        console.log("March");
+        break;
+        
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case Match");
+        break;
+}
